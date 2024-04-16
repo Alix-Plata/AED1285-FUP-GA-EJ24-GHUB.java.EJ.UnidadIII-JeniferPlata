0ac6ff86d6e5821ccdd1f1701f8a5cd5dce3ba8e
Jenifer Plata <159405318+Alix-Plata@users.noreply.github.com>
2024-04-16 15:02:11 +0000

    clase romano

---
+++ miPrincipal/romano/romano.java
@@ -0,0 +1,9 @@
+//Propocito;convertir uncaracter (a,e,i,o,u) en un numero
+//donde a la a-1 e-2 i-3 o-4 u-5
+// autor:Plata Rdriguez
+//Fecha:16/04/24
+import java.util.Scanner;
+public class romano{
+    //definir objeto scanner
+    Scanner teclado = new 
+}
