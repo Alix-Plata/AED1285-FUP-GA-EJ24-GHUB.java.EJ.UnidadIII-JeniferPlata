f4d37522d8e80679038b39553669c36d34b4b935
Jenifer Plata <159405318+Alix-Plata@users.noreply.github.com>
2024-04-17 15:02:33 +0000

    actualizacion 17/04/247

--- miPrincipal/romano/romano.java
+++ miPrincipal/romano/romano.java
@@ -5,5 +5,27 @@
 import java.util.Scanner;
 public class romano{
     //definir objeto scanner
-    Scanner teclado = new 
+    Scanner teclado = new Scanner(System.in);
+    //definir variable de salida 
+    string roma;
+    System.out.println("Proporciona numero arabigo (1-10): ");
+    ara = entrada.nextShort();
+    Switch (ara){
+        case 1:
+        roma = "I"
+        case 2:
+        roma = "II"
+        case 3:
+        roma = "III"
+        case 4:
+        roma = "IV"
+        case 5:
+        roma = "V"
+    }
+}
+
+
+    
+}
+
 }
